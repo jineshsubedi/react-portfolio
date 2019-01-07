bea119bdd2691e8bcebb3e7b17f07710fd7fce3e
jinesh1094 <jinesh.3c@gmail.com>
2019-01-07 15:39:19 +0545

    Updates

---
+++ precache-manifest.3009f90ef1cff8cbe5b224943e03b328.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f6bbc3de7bc14b8e7ff2df92f25ef45",
+    "url": "/react-portfolio/static/media/banner.2f6bbc3d.jpg"
+  },
+  {
+    "revision": "71ce8d5941d0976acf13e93011be1f73",
+    "url": "/react-portfolio/static/media/about.71ce8d59.jpg"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "e9d9b20abea3d59ae4be",
+    "url": "/react-portfolio/static/js/main.e9d9b20a.chunk.js"
+  },
+  {
+    "revision": "b7b88a9e0eb96c7549de",
+    "url": "/react-portfolio/static/js/1.b7b88a9e.chunk.js"
+  },
+  {
+    "revision": "e9d9b20abea3d59ae4be",
+    "url": "/react-portfolio/static/css/main.4352b5b6.chunk.css"
+  },
+  {
+    "revision": "b7b88a9e0eb96c7549de",
+    "url": "/react-portfolio/static/css/1.2f4528e4.chunk.css"
+  },
+  {
+    "revision": "849a36356bca7d1b234a1efe7d738b16",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/public/precache-manifest.7fbdf10bbde1833f50d64a91f62b9159.js"
+  "/react-portfolio/precache-manifest.3009f90ef1cff8cbe5b224943e03b328.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/public/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
